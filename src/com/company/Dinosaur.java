98110a6a0b23e0913df99ecb9da4b38a6772dea8
ox6185ys <ox6185ys@go.minneapolis.edu>
2016-09-20 11:29:14 -0500

    GIT Creation Week5 Dinosaur Roar message

---
+++ src/com/company/Dinosaur.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by ox6185ys on 9/20/2016.
+ */
+public class Dinosaur {
+
+    public void roar(){
+        System.out.println("GRRRR!!");
+    }
+}
---
+++ src/com/company/DinosaurSupervisor.java
@@ -0,0 +1,11 @@
+package com.company;
+public class DinosaurSupervisor{
+    public static void main(String[] args) {
+        Dinosaur t_rex = new Dinosaur();
+        Dinosaur velociraptor = new Dinosaur();
+        Dinosaur triceratops = new Dinosaur();
+        t_rex.roar();
+        velociraptor.roar();
+        triceratops.roar();
+    }
+}
